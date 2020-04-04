a46f5a8cafb4b10ee802ec2699768fbdeed7db65
forcheng <2871979271@qq.com>
2020-04-04 17:51:21 +0800

    forcheng - add product details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.compoent'
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component'
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
@@ -14,17 +15,19 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.compoent
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent }
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent,
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
 
 
 /*
